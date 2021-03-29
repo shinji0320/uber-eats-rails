669d4134b419526a0807137e18366f893ce23837
shinji0320 <shinji.blue.one@gmail.com>
2021-03-29 17:17:09 +0900

    [finish]Foods

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,49 +1,115 @@
-import React, { Fragment, useEffect, useReducer } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
 
-//reducers
+
+// components
+import { LocalMallIcon } from '../components/Icons';
+
+import { FoodWrapper } from '../components/FoodWrapper';
+import Skeleton from '@material-ui/lab/Skeleton';
+
+// reducers
 import {
   initialState as foodsInitialState,
   foodsActionTyps,
   foodsReducer,
 } from '../reducers/foods';
 
-//api
+// apis
 import { fetchFoods } from '../apis/foods';
 
-//constants
+// images
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+
+// constants
+import { COLORS } from '../style_constants';
 import { REQUEST_STATE } from '../constants';
 
-export const Foods = ({ match }) => {
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
+
+export const Foods = ({
+  match
+}) => {
   const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
 
   useEffect(() => {
-    dispatch({ type: foodsActionTyps.FETCHING});
+    dispatch({ type: foodsActionTyps.FETCHING });
     fetchFoods(match.params.restaurantsId)
-    .then((data) =>
-      dispatch({
-        type: foodsActionTyps.FETCH_SUCCESS,
-        payload: {
-          foods: data.foods
-        }
+      .then((data) => {
+        dispatch({
+          type: foodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
       })
-    )
-  }, [])
+  }, []);
+
   return (
     <Fragment>
-       {
-        foodsState.fetchState === REQUEST_STATE.LOADING ?
-          <Fragment>
-            <p>
-              ロード中...
-            </p>
-          </Fragment>
-        :
-          foodsState.foodsList.map(food =>
-            <div key={food.id}>
-              {food.name}
-            </div>
-          )
-      }
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </Fragment>
+          :
+            foodsState.foodsList.map(food =>
+              <ItemWrapper key={food.id}>
+                <FoodWrapper
+                  food={food}
+                  onClickFoodWrapper={(food) => console.log(food)}
+                  imageUrl={FoodImage}
+                />
+              </ItemWrapper>
+            )
+        }
+      </FoodsList>
     </Fragment>
   )
 }

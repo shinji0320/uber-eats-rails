75428a9354651e88d374961d3fe82eb3afede3f8
shinji0320 <shinji.blue.one@gmail.com>
2021-03-29 16:01:32 +0900

    [Link]foods[show]

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,14 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 
-export const Foods = ({
-  match
-}) => {
+//api
+import { fetchFoods } from '../apis/foods';
+
+
+export const Foods = ({ match }) => {
+  useEffect(() => {
+    fetchFoods(1)
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
   return (
     <Fragment>
       フード一覧
-      <p>
-      restaurantsIdは {match.params.restaurantsId} です
-      </p>
     </Fragment>
   )
 }

6d11f0f947fb40e209b91a5fa88f1fb9af7bc145
shinji0320 <shinji.blue.one@gmail.com>
2021-03-29 16:37:12 +0900

    foods/:id

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,19 +1,49 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+//reducers
+import {
+  initialState as foodsInitialState,
+  foodsActionTyps,
+  foodsReducer,
+} from '../reducers/foods';
 
 //api
 import { fetchFoods } from '../apis/foods';
 
+//constants
+import { REQUEST_STATE } from '../constants';
 
 export const Foods = ({ match }) => {
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
   useEffect(() => {
-    fetchFoods(1)
+    dispatch({ type: foodsActionTyps.FETCHING});
+    fetchFoods(match.params.restaurantsId)
     .then((data) =>
-      console.log(data)
+      dispatch({
+        type: foodsActionTyps.FETCH_SUCCESS,
+        payload: {
+          foods: data.foods
+        }
+      })
     )
   }, [])
   return (
     <Fragment>
-      フード一覧
+       {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <Fragment>
+            <p>
+              ロード中...
+            </p>
+          </Fragment>
+        :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          )
+      }
     </Fragment>
   )
 }

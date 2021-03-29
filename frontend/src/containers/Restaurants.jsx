cc4739c62b415f886bfa7b749ab0612a71ece40a
shinji0320 <shinji.blue.one@gmail.com>
2021-03-29 13:20:48 +0900

    [join]forntend/backend

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { fetchRestaurants } from '../apis/restaurants'; // apis
 
 export const Restaurants = () => {
+
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
   return (
     <Fragment>
       レストラン一覧
